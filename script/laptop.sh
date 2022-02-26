52789ac666e897db5e0161abf8e48d82864def2f
mylovefeelsallwrong <96679129+mylovefeelsallwrong@users.noreply.github.com>
2022-02-25 21:29:58 -0500

    Add files via upload

---
+++ script/laptop.sh
@@ -0,0 +1,5 @@
+#i!/bin/bash
+xrandr --output  eDP1 --auto --primary
+xrandr --output HDMI1 --off 
+echo "monitor off,laptop on"
+
---
+++ script/monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xrandr --output  eDP1 --off
+xrandr --output HDMI1 --auto --primary
+echo "laptop off, monitor on"
+
